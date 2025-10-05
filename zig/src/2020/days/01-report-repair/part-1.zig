fd98c20b0b1afe8e906a0da5977a520943015826
Patrick Konrad Piel <patrick@prismaticweb.io>
2025-10-05 11:58:17 +0200

    add solution 2020 day 1

---
+++ zig/src/2020/days/01-report-repair/part-1.zig
@@ -0,0 +1,52 @@
+const std = @import("std");
+const heap = std.heap;
+const mem = std.mem;
+const fs = std.fs;
+const io = std.Io;
+
+fn checkSum2020(a: i32, b: i32) bool {
+    return a + b == 2020;
+}
+
+fn readInput(allocator: mem.Allocator) ![]i32 {
+
+    // open/ close file so we can read the content
+    const file = try fs.cwd().openFile("input.txt", .{});
+    defer file.close();
+
+    // getting the exact size of the file
+    const file_size = try file.getEndPos();
+    const content = try allocator.alloc(u8, file_size);
+    defer allocator.free(content);
+
+    // has to be var because the reader changes state (see reader.read())
+    var reader = file.reader(&.{});
+    _ = try reader.read(content);
+
+    var records = std.array_list.Aligned(i32, null).empty;
+    var lines = std.mem.splitScalar(u8, content, '\n');
+
+    while (lines.next()) |line| {
+        try records.append(allocator, try std.fmt.parseInt(i32, line, 10));
+    }
+
+    return try records.toOwnedSlice(allocator);
+}
+
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const allocator = gpa.allocator();
+
+    const entries = try readInput(allocator);
+    defer allocator.free(entries);
+
+    for (entries, 0..) |entry, i| {
+        for (entries, i + 1..) |other_entry, j| {
+            if (checkSum2020(entry, other_entry)) {
+                std.debug.print("Found entries at {d} and {d}: {d} + {d} = 2020\n", .{ i, j, entry, other_entry });
+                std.debug.print("The product is: {d}\n", .{entry * other_entry});
+            }
+        }
+    }
+}
